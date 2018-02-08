1b0357865a33f8223c240bf3f92858b35d9057fb
Paul Souvestre <paul@qonto.eu>
2018-02-08 13:21:52 +0100

    Boiler plate

---
+++ tts/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style lang="scss" src='@/style/main.scss'></style>
---
+++ tts/src/components/home/Home.vue
@@ -0,0 +1,13 @@
+<template src='@/components/home/home.html'></template>
+<style lang="scss" src='@/components/home/home.scss'></style>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
