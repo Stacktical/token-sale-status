883655b42a5bba1b850e48594485a406a62d3b06
Paul Souvestre <paul@qonto.eu>
2018-02-09 16:36:10 +0100

    Etherscan first integration - get all ETH from a contract

--- tts/src/components/home/Home.vue
+++ tts/src/components/home/Home.vue
@@ -1,13 +1,19 @@
 <template src='@/components/home/home.html'></template>
 
-<style lang="scss" src='@/components/home/home.scss'></style>
 <style lang="scss" src='@/style/main.scss'></style>
+<style lang="scss" src='@/components/home/home.scss'></style>
 
 <script>
+
+import ProgressBar from '@/components/progress-bar/Progress-bar'
+
 export default {
   name: 'Home',
   data () {
     return {}
+  },
+  components: {
+    ProgressBar
   }
 }
 </script>
---
+++ tts/src/components/progress-bar/Progress-bar.vue
@@ -0,0 +1,27 @@
+<template src='@/components/progress-bar/progress-bar.html'></template>
+
+<style lang="scss" src='@/components/progress-bar/progress-bar.scss'></style>
+
+<script>
+
+import EtherService from '@/components/progress-bar/progress-bar.service.js'
+
+export default {
+  name: 'Prgress-Bar',
+  data () {
+    return {
+      icoAddress: '0x9df3a24d738ae98dea766cd89c3aef16583a4daf',
+      ethRaised: 0,
+    }
+  },
+  created () {
+    console.log('Created progres bar')
+    EtherService.getTotalEthBalance(this).then((response) => {
+      this.ethRaised = (Number(response.body.result) / 1e18);
+    })
+  },
+  methods: {
+
+  }
+}
+</script>
