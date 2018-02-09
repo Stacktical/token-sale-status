1a8037ac9bd78e7588495f8ab16f51c38a9c6a0f
Paul Souvestre <paul@qonto.eu>
2018-02-09 15:09:42 +0100

    Homepage ready for progress bar + etherscan integration

--- tts/src/components/home/Home.vue
+++ tts/src/components/home/Home.vue
@@ -1,5 +1,7 @@
 <template src='@/components/home/home.html'></template>
+
 <style lang="scss" src='@/components/home/home.scss'></style>
+<style lang="scss" src='@/style/main.scss'></style>
 
 <script>
 export default {
