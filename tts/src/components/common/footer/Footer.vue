10d14b60e7e426c09ccb456f63d96afb2ff6bd6d
Paul Souvestre <paul@qonto.eu>
2018-02-11 16:22:58 +0100

    Footer

---
+++ tts/src/components/common/footer/Footer.vue
@@ -0,0 +1,18 @@
+<template src='@/components/common/footer/footer.html'></template>
+
+<style lang="scss" src='@/components/common/footer/footer.scss'></style>
+
+<script>
+
+export default {
+  name: 'Footer',
+  props: [],
+  data () {
+    return {}
+  },
+  computed: {
+  },
+  methods: {
+  }
+}
+</script>
